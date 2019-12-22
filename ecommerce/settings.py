d167e3d8afb382b44635034b390f3efacc8758bb
Luana Andrade <luzi015@hotmail.com>
2019-12-22 15:13:50 +0000

    heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,7 +150,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'storage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -158,7 +158,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
